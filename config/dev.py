cb3d5addf0581d39e67240b5cba75c78f4e7f910
payton-yang <payton.yang@bundleb2b.net>
2022-01-23 23:35:13 +0800

    Fix: api

--- config/dev.py
+++ config/dev.py
@@ -1,6 +1,6 @@
 from .settings import *
 
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['127.0.0.1', '116.62.27.195']
 APPEND_SLASH = False
 DATABASES = {
--- web/urls.py
+++ web/urls.py
@@ -4,7 +4,7 @@ from .views import TestView, FeedbackView, TestAsyncTaskView
 
 app_name = 'web'
 urlpatterns = [
-    path('test/', TestView.as_view()),
-    path('feedback/', FeedbackView.as_view()),
-    path('async/', TestAsyncTaskView.as_view()),
+    path('test', TestView.as_view()),
+    path('feedback', FeedbackView.as_view()),
+    path('async', TestAsyncTaskView.as_view()),
 ]
