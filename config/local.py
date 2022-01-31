2b41670098d31f45413f57c7478904c74eccfce4
payton-yang <payton.yang@bundleb2b.net>
2022-01-31 15:48:12 +0800

    Feat: init project

---
+++ config/local.py
@@ -0,0 +1,16 @@
+import os
+
+from .settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'test2',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': 5432,
+    }
+}
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
